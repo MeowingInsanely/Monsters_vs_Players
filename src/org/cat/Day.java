ae228b5dcf6edba3551fe9e3b5869eab1174a482
MeowingInsanely <ninared2@gmail.com>
2017-07-25 00:17:49 +0800

    Added Night & Edited Day
    Day at //vote
    Added Night.java

--- src/org/cat/Day.java
+++ src/org/cat/Day.java
@@ -7,8 +7,51 @@ package org.cat;
 
 /**
  *
- * @author Nina
+ * @author MeowingInsanely
  */
-public class Day {
-    
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Day extends BukkitRunnable {
+
+    World world;
+
+    int countdown = 40;
+
+    public Day() {
+        world = ???.getTheWorld();
+        world.setTime(23000);
+    }
+
+    public void run() {
+        world.setTime(world.getTime() + 140);
+
+        // Time change (24,000 is the same as 0);
+        if (world.getTime() >= 24000) {
+            world.setTime(world.getTime() - 24000);
+        }
+
+        // TODO: Vote
+        if (world.getTime() >= 4600) {
+            getLogger().info("Vote time has begun")
+        }
+        // TODO: Defense
+        // TODO: Judgement
+        // TODO: Last Words
+        // TODO: Murder
+        // TODO: Send Graveyard
+        // TODO: Will
+        // TODO: Role
+        // TODO: Repeat -> Vote
+        // TODO: Night
+        
+        // Tell the time to everybody (Spectators???)
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            player.setLevel(countdown);
+        }
+        // Countdown
+        countdown--;
+    }
 }
