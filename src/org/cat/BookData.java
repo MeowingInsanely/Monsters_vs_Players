89bec9c3160df43c0dd6543eb2a288f15f58a7c9
stefnotch <brandmairstefan@gmail.com>
2017-07-30 19:39:39 +0200

    Fixed imports

--- src/org/cat/BookData.java
+++ src/org/cat/BookData.java
@@ -12,10 +12,15 @@ import org.bukkit.inventory.ItemStack;
 //import org.bukkit.ChatColor;
 //import org.bukkit.Material;
 //import org.bukkit.inventory.ItemStack;
+//
+// https://bukkit.gamepedia.com/Main_Page
 //import org.bukkit.inventory.meta.BookMeta;
 
 
 public class BookData implements CommandExecutor {
+    //https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/inventory/meta/BookMeta.html
+    //https://bukkit.org/threads/book-meta.131816/
+    //https://github.com/stefnotch/stefnotch.github.io/blob/master/TOS/src/main/java/io/github/stefnotch/tos/TOS.java#L194
     
     @Override
     public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
